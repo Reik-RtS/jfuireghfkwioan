5025191297e47ae7ed4b5fe67ad4b307a333b30f
Reik-RtS <59104654+Reik-RtS@users.noreply.github.com>
2024-08-19 16:50:59 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC4sLp_eOeQkEVl4LRHcdZ84PjgyPUgZlE",
+    authDomain: "jfuireghfkwioan.firebaseapp.com",
+    databaseURL: "https://jfuireghfkwioan-default-rtdb.firebaseio.com",
+    projectId: "jfuireghfkwioan",
+    storageBucket: "jfuireghfkwioan.appspot.com",
+    messagingSenderId: "700003578134",
+    appId: "1:700003578134:web:b8a4c91fe2c779b55c2037"
+};
+
+firebase.initializeApp(firebaseConfig);
